b749ccefcece16a6a20c964d9ee955626058e1c3
biancaglez <bianca.glez94@gmail.com>
2020-03-17 16:30:01 -0600

    # more plant names in excel file + other script updates

--- Code/Data Management.R
+++ Code/Data Management.R
@@ -22,18 +22,20 @@ d_19_origins <- #Origins data for the new 2019 species
   #These origin data were assigned by Taylor Edwards and Bianca Gonzalez in 2020
   read.csv("./Data/Species/Dome_newspp_origins.2019.csv")
 
-####2. Process data####
+####2. Process data 2013 and 2019 exotics ####
 d_13$Origin[d_13$Origin=="NA"] <- NA #Recode NA from text to NA
 
 #Define exotics and native species list, and assign to d:
 exotics <- unique(d_13$Code[which(d_13$Origin=="exotic")])
 exotics <- c(exotics, 
              unique(d_19_origins$Code[which(d_19_origins$Origin=="exotic")]))
+
 natives <- unique(d_13$Code[which(d_13$Origin=="native")])
 natives <- c(natives, 
              unique(d_19_origins$Code[which(d_19_origins$Origin=="native")]))
 d$Origin[d$Code%in%exotics] <- "exotic" 
 d$Origin[d$Code%in%natives] <- "native"
+
 
 #Convert transect ID's to be consistently lower-case
 d$Transect_id <- tolower(d$Transect_id)
@@ -43,7 +45,6 @@ d$Transect_id <- tolower(d$Transect_id)
 d[which(d$Transect_id=="3LAU1"),"RevBI"] <- "Unburned" #Alternatively, lump in with "unburned"
 #Investigate multiple burn severity issue 
 #(CHECKME_JTS this was a comment from earlier, not sure if resolved)
-
 
 
 ####3. Simplify non-living codes####
@@ -70,56 +71,57 @@ d[d$Full_name== "pine cone", "Full_name"] <- "litter" #CHECKME_JTS: Kay are pine
 
 ####4. Deal with name changes####
 ## BG to finish
-#Create list of name changes (code_old, fullname_old, code_new etc)
+#Create list of name changes (code_old, fullname_old, code_new, fullname_new)
 
-#Duplicate columns
-d$fullname_old <- tolower(d$Full_name)
-d$code_old <- d$Code #BG Checkme: Can I rename this "code_old"? it was "cold_old".
-
-# add other columns to fill out later
-#d[c("fullname_new", "code_new")] <-NA
-
-# let's make sure shortlist doesn't have mispellings in dataset and there are only two! 
-d[grep("villosa", d$Full_name, ignore.case = T),] %>% select("Full_name") %>% unique()
-
-# make a dataframe of characters to loop through to check partial name matches - can also import when Kay sends
-shortlist_partial <- c("neomexicana", "villosa")
-
-# Shortlist (already converted to READable format from txt file recieved)
+# Shortlist (already converted to excel from txt file)
 short_lst <- read_excel("./Data/new_sci_names_DOME_plants.xlsx", trim_ws = T)
 
-short_lst$first <- stringr::word(short_lst$Old, 1)
+#Duplicate columns
+d$fullname_old <- as.character(tolower(d$Full_name))
+d$code_old <- as.character(tolower(d$Code))
+
+# let's make sure shortlist doesn't have variations of a plant name
+
+### investigating results using ITIS integrated Taxonomic Information System
+
+short_lst$first <- stringr::word(short_lst$New, 1)
+short_lst$last <- stringr::word(short_lst$New, 2)
+
+list_firstnames = list()
+list_secondnames = list()
+
+#### check out other possible name combos from the old names in shortlist  
 
 for(i in 1:nrow(short_lst)){
-  d[grep(short_lst[i,4], d$Full_name, ignore.case = T),] %>% select("Full_name") %>% unique() %>% print()  
-}
+  
+  #make sure there are no mispellings or variations of plant from shortlist -- correct if there are
+  list_firstnames[[i]] <- d[grep(short_lst[i,"first"], d$Full_name, ignore.case = T),] %>% select("Full_name") %>% unique() 
+  list_secondnames[[i]] <- d[grep(short_lst[i,"last"], d$Full_name, ignore.case = T),] %>% select("Full_name") %>% unique() 
+  }
 
+# interested in multiple variations of a name for first/second words in shortlist 
+list_firstnames <- list_firstnames %>% 
+  purrr::discard(function(x) nrow(x) <= 1)
 
-#### with above list check out other possible name combos that they can be under and add to list
-
-# initalize column
-d$fullname_new <- NA
+list_secondnames <- list_secondnames %>% 
+  purrr::discard(function(x) nrow(x) <= 1)
 
 # Insert new names
-#BG_CHECKME: the next 5 lines (102-106) take the place of the for loops on lines ##-##. This process is easier without using pipes. Delete this comment once you understand the new code.
-d$fullname_new <- #Add new name to species where old name is flagged to have changed:
-  tolower(short_lst$new)[pmatch(d$fullname_old,tolower(short_lst$Old),
-                                                       duplicates.ok = TRUE)]
+#Add new name to species where old name is flagged to have changed:
+d$fullname_new <- tolower(short_lst$New)[pmatch(d$fullname_old,tolower(short_lst$Old), 
+                                                       duplicates.ok = TRUE)] 
+
+# gives indices of short_lst$old that match the old ones and returns -- when we search that index []
+# in the new short_list$new -- they correspond & hence can replace w new vals 
 
 d$fullname_new[is.na(d$fullname_new)] <- d$fullname_old[is.na(d$fullname_new)]
 
-# import old names for species that didn't change
+# need code_new column now -- use same pattern matching technique ^
 
-# Little DF from stack overflow to start off the naming
-df<-data.frame(matrix(NA, nrow = 1, ncol = 2,
-                  dimnames = list(NULL, paste0("new_Name", 1:2))) ) %>% dplyr::rename(Old_Name = new_Name1)
-df$Old_Name<- "Chrysopsis villosa"
-df$new_Name2 <- "Heterotheca villosa"
+d$code_new <- tolower(short_lst$new_code)[pmatch(d$code_old, as.character(tolower(short_lst$old_code)),
+                                                 duplicates.ok = T)]
 
-# need codes
-
-# write the file to compare with KAY and get her opinion
-write.csv(d, paste0(getwd(), "/", "name_changes.csv"))
+d$code_new[is.na(d$code_new)] <- d$code_old[is.na(d$code_new)]
 
 #to use for crosswalk with master data "d"
 #Do the crosswalk and 
@@ -127,16 +129,25 @@ write.csv(d, paste0(getwd(), "/", "name_changes.csv"))
 #then move on to step 5.
 #A
 
+
 ####5. Deal with unknown orgins####
 
 #Flag plants that don't have origin. Develop a reconciliation document for these:
 d$RowNum <- c(1:nrow(d)) #for eventual sorting
+
+no_origin_master<- d$Full_name[is.na(d$Origin)]
+
+# can make an object or filter in excel
+ssp_origins <- d[grepl("spp", d$Full_name, ignore.case = T),]
+
+# reconciliation 
+write.csv(no_origin, "./Data/BiancaReconciliation/Reconciliation_no_origins.csv")
+
 #write.csv(d, "Data/Tmp/Reference1.csv")
 issues <- d[is.na(d$Origin) & d$Plant=="y",
             c("RowNum","Transect_id", "year", "Total_cm", "Code", "Full_name", "Origin")]
 issues$Code_new <- "unchanged"
 issues$Full_name_new <- "unchanged"
-#issues$Code_old <- ""	#Not doing this, the name changes are being dealt with separately
-#issues$Full_name_old <- ""
-#write.csv(issues, "Data/BiancaReconciliation/Reconciliation1.csv")
+
+
 
