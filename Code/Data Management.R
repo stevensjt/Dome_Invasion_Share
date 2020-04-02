d6615e2aac63bb77165c55deb09c096cbda91df6
Jens Stevens <jtstevens@usgs.gov>
2020-04-01 21:10:34 -0600

    File reorganization and updating/cleaning Data Management script to prepare for syncing Bianca's edited issues file with the master data file.

--- Code/Data Management.R
+++ Code/Data Management.R
@@ -14,14 +14,20 @@ library(readr)
 d_13 <- #Old data through 2013
   #These data have been assigned an origin (native vs exotic), by Rebecca Oertel?
   #The main point of reading in this file is to extract the origin data.
-  read_excel("./Data/dome_invasion_data.xlsx", sheet = "data") 
-d <- #Data through 2019
-  #These data contain measurements from 2016 and 2019 that are absent from the previous file
+  read_excel("./Data/KayOriginalMaster/dome_invasion_data.xlsx", sheet = "data") 
+d <- #Data through 2019.
+  #These data contain measurements from 2016 and 2019 that are absent from the previous file d_13,
+  #Except that species that appeared in the dataset for the first time in 2019, 
+    #either because they were newly identified or because their name changed,
+    #had their origin data were assigned by Taylor Edwards and Bianca Gonzalez in 2020.
+    #Apparently there were no new species in 2016
+  #As with the raw data file file All Years 1997-2019 PermPlot-Entire.xlsx, 
+    #the transect 3mbu2a was sampled only in 2002 and according to Kay is irrelevant data,
+    #and has been deleted.
   #These data will become the template for the master data file
-  read_excel("./Data/All Years 1997-2019 PermPlot-Entire.xlsx", sheet = "97-2019 EX1") 
-d_19_origins <- #Origins data for the new 2019 species
-  #These origin data were assigned by Taylor Edwards and Bianca Gonzalez in 2020
-  read.csv("./Data/Species/Dome_newspp_origins.2019.csv")
+  #read_excel("./Data/All Years 1997-2019 PermPlot-Entire.xlsx", sheet = "97-2019 EX1") #Deprecate
+#d_19_origins <- #Origins data for the new 2019 species Deprecate
+  read_csv("./Data/Dome_newspp_origins.2019.csv", col_types = cols())
 
 ####2. Process data 2013 and 2019 exotics ####
 d_13$Origin[d_13$Origin=="NA"] <- NA #Recode NA from text to NA
@@ -29,23 +35,21 @@ d_13$Origin[d_13$Origin=="NA"] <- NA #Recode NA from text to NA
 #Define exotics and native species list, and assign to d:
 exotics <- unique(d_13$Code[which(d_13$Origin=="exotic")])
 exotics <- c(exotics, 
-             unique(d_19_origins$Code[which(d_19_origins$Origin=="exotic")]))
+             unique(d$Code[which(d$Origin=="exotic")]))
 
 natives <- unique(d_13$Code[which(d_13$Origin=="native")])
 natives <- c(natives, 
-             unique(d_19_origins$Code[which(d_19_origins$Origin=="native")]))
+             unique(d$Code[which(d$Origin=="native")]))
 d$Origin[d$Code%in%exotics] <- "exotic" 
 d$Origin[d$Code%in%natives] <- "native"
-
+rm(d_13) #done with the 2013 data frame.
 
 #Convert transect ID's to be consistently lower-case
 d$Transect_id <- tolower(d$Transect_id)
 
-#Burn severity issues
-#d <- d[-which(d$RevBI=="Low"),] #Remove the only transect in low severity (N=1; 3LAU1)
-d[which(d$Transect_id=="3LAU1"),"RevBI"] <- "Unburned" #Alternatively, lump in with "unburned"
-#Investigate multiple burn severity issue 
-#(CHECKME_JTS this was a comment from earlier, not sure if resolved)
+#Address burn severity issues
+#d <- d[-which(d$RevBI=="Low"),] #Remove the only transect in low severity (N=1; 3lau1)
+d[which(d$Transect_id=="3lau1"),"RevBI"] <- "Unburned" #Alternatively, lump in with "unburned"
 
 
 ####3. Simplify non-living codes####
@@ -66,25 +70,22 @@ d[d$Full_name== "combined soil,cryp,pumc,grav,rock", "Full_name"] <- "Bare"
 d[d$Full_name== "combined soil,pumc,grav,rock", "Full_name"] <- "Bare"
 d[d$Full_name== "Soil", "Full_name"] <- "Bare"
 d[d$Full_name== "Bare", "Code"] <- "bare"
-d[d$Full_name== "pine cone", "Code"] <- "litt"
-d[d$Full_name== "pine cone", "Full_name"] <- "litter" #CHECKME_JTS: Kay are pine cones called litter now?
+d[d$Full_name== "pine cone", "Code"] <- "litt" #Confirmed with Kay that pine cones are now called litter
+d[d$Full_name== "pine cone", "Full_name"] <- "litter" 
 #DONE
 
 ####4. Deal with name changes####
-## BG to finish
+## BG did this part
 #Create list of name changes (code_old, fullname_old, code_new, fullname_new)
 
 # Shortlist (already converted to excel from txt file)
-short_lst <- read_excel("./Data/new_sci_names_DOME_plants.xlsx", trim_ws = T)
+short_lst <- read_excel("./Data/Species/new_sci_names_DOME_plants.xlsx", trim_ws = T)
 
 #Duplicate columns
 d$fullname_old <- as.character(tolower(d$Full_name))
 d$code_old <- as.character(tolower(d$Code))
 
 # let's make sure shortlist doesn't have variations of a plant name
-
-### investigating results using ITIS integrated Taxonomic Information System
-
 short_lst$first <- stringr::word(short_lst$New, 1)
 short_lst$last <- stringr::word(short_lst$New, 2)
 
@@ -92,7 +93,6 @@ list_firstnames = list()
 list_secondnames = list()
 
 #### check out other possible name combos from the old names in shortlist  
-
 for(i in 1:nrow(short_lst)){
   
   #make sure there are no mispellings or variations of plant from shortlist -- correct if there are
@@ -124,14 +124,9 @@ d$code_new<- paste(substr(d$fullname_new, start = 1, stop = 2),
 # if nonplant - replace new code
 d<- d %>% mutate(code_new= ifelse(d$Plant =='y', d$code_new, d$code_old))
 
-#to use for crosswalk with master data "d"
-#Do the crosswalk and
-#Also do the crosswalk with "d_13"
-#then move on to step 5.
-#A
 
 
-####5. Deal with unknown orgins####
+####5. Deal with unknown orgins and other issues####
 
 #Flag plants that don't have origin. Develop a reconciliation document for these:
 d$RowNum <- c(1:nrow(d)) #for eventual sorting
@@ -139,39 +134,49 @@ d$RowNum <- c(1:nrow(d)) #for eventual sorting
 # could be new plants or non plants -- or only identified to genus -- 
 #no_origins <- d[is.na(d$Origin),]
 
-# can make an object or filter in excel
-#ssp_origins <- d[grepl("spp", d$Full_name, ignore.case = T),]
 
-# reconciliation 
-
-#write.csv(d, "Data/Tmp/Reference1.csv")
 issues <- d[is.na(d$Origin) & d$Plant=="y",
             c("RowNum","Transect_id", "year", "Total_cm", "Code", "Full_name", "Origin")]
-issues$Code_new <- "unchanged"
-issues$Full_name_new <- "unchanged"
+issues$code_new <- "unchanged"
+issues$fullname_new <- "unchanged"
+#write.csv(issues, "Data/BiancaReconciliation/issues.csv") #DON'T DO THIS, HAS BEEN DONE
 
-
-##JTS_CHECKME:
+##IMPORTANT: At this point Bianca did a bunch of manual checking of the data in the issues document
+##This document "issues.csv" above ended up being named "issues-jens.csv". 
+##The reason "issues-jens" has 1107 rows and the "issues" doc created here only has 994 is that
+##the doc created in R doesn't include the new 2019 species (because Origin is not NA) but 
+##when Jens created the earlier version that became "issues-jens", the version of d used to create
+##the object "issues' above somehow didn't have the 2019 Origins added in. That was Jens' mistake somehow.
+##In any case, the "issues" doc will be reconciled with "d" below.
 
 # How to match rownums of recon ^ with the d dataframe and replace origin vals if they match
 # Can you write this using pmatch and/or which? I'd like some more exposure to this kind of 
 # code 
-
-
-?pmatch()
-?which()
+#?pmatch()
+#?which()
 
 #JTS for BG: Here is the answer to your question above:
+
+####6. Incorporate the fixed issues into "d"####
+#JTS: Start Here (and eventually deprecate the commented lines immediately preceding.)
 issues_fixed <- #Need to use "read_csv" rather than "read.csv" to keep columns as characters. Requires readr
   read_csv("./Data/BiancaReconciliation/issues-Jens.csv") 
 issues_fixed <- #IMPORTANT: Because this document had been sorted alphabetically, it needs to be re-sorted by RowNum.
   issues_fixed[order(issues_fixed$RowNum),] 
-issues_fixed$Code <- #Migrated "Code_new" into "Code" in issues doc, IF it changed
-  ifelse(issues_fixed$Code_new=="unchanged",issues_fixed$Code,issues_fixed$Code_new)
-issues_fixed$Full_name <- #Migrated "Full_name_new" into "Code" in issues doc, IF it changed
-  ifelse(issues_fixed$Full_name_new=="unchanged",issues_fixed$Full_name,issues_fixed$Full_name_new)
+issues_fixed$Code <- #Migrated "code_new" into "Code" in issues doc, IF it changed
+  ifelse(issues_fixed$code_new=="unchanged",issues_fixed$Code,issues_fixed$code_new)
+issues_fixed$Full_name <- #Migrated "fullname_new" into "Code" in issues doc, IF it changed
+  ifelse(issues_fixed$fullname_new=="unchanged",issues_fixed$Full_name,issues_fixed$fullname_new)
 tmp_d <- d #Temporary data frame to do the integration, this will eventually get written to file but leave that for Jens to do.
 tmp_d[which(tmp_d$RowNum%in%issues_fixed$RowNum),c("Code","Full_name","Origin")] = 
   issues_fixed[,c("Code","Full_name","Origin")]
 #Now compare d to tmp_d for quality control to make sure the three columns in question were changed appropriately. 
+
+
+##Todo: Develop comprehensive species list by year to see if there were any new species in 2016 and what happened to their origins data.
+#Origins need to be imported from issues_fixed for all row matches into d.
+#Check for duplicate species from the same transect-year
+#There is no Frovaria sp ANYWHERE ON THE INTERNET. Fragaria?
+#Cryptogram should not be a plant.
+#At the end, Kay should check for genera that must be native even if unknown (like Carex).
 
